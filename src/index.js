7c6e35d73e802ddd89bace92b4a26f66b5b34ca7
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-08-03 08:51:22 -0500

    Preparando vistas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 // Initializacions
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views', 'layouts')),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 
